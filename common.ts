5342048d0668609d14cbcbfc6671ec24e8c943d1
Ayush Singh <ayush-_-singh@live.com>
2022-01-29 16:56:31 +0530

    Delete common.ts

--- common.ts
+++
@@ -1,9 +0,0 @@
-import { File, Observable } from '@nativescript/core';
-
-export abstract class NativescriptDownloaderCommon extends Observable {
-    constructor() {
-        super();
-      }
-      public abstract download(options: any): any;
-      
-}
